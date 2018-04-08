f00964b9e7f12d7ce901fdcc2700b02a49b9d8f4
radkoncyAir <radkoncy@gmail.com>
2018-04-08 13:37:35 +0100

    working counter as get method /increment

--- app.js
+++ app.js
@@ -7,53 +7,63 @@ var fs = require('fs');
 var counter = 0;
 var counterFileName = "counter.txt";
 
-function incrementCounterInFile(){
-    var stats = fs.stat(counterFileName, function(err, stats){
+(function (){
+    console.log("Initialization started...");
+
+    fs.stat(counterFileName, function(err, stats){
         if(err){
             if(err.code == 'ENOENT') {
-                // file does not exist
-                fs.writeFile(counterFileName, 1);
-                console.log('file created with 1 as starting value');
+                fs.writeFile(counterFileName, 1, function(err){
+                    if(err) 
+                        throw err;
+                });
+                console.log('File created with 1 as a starting value');
             }else{
-                console.log("error!!!1 + " + err);
+                console.log("Error when checking file. Error: " + err);
                 throw err;
             }
-        }else{
-            console.log('file exist');
-            fs.readFile(counterFileName, function(err, data){
-                if(err)
-                    throw err;
-
-                if(data){
-                    console.log(data.toString());
-                    
-                    fs.writeFile(
-                        counterFileName, 
-                        parseInt(data.toString())+1, 
-                        function(err){
-                            if(err != null)
-                                throw err;
-                        }
-                    );
-                }
-            });
         }
-    })
-    //path.exists('counter.txt', function(){
+    });
 
-    //});
-    
-//    var fs = require('fs');
-    
+    console.log("Initialization completed!");
+}());
+
+function incrementCounterInFile(res){
+    fs.readFile(counterFileName, function(err, data){
+        if(err) 
+            throw err;
+
+        if(data){
+            console.log(data.toString());
+            
+            fs.writeFile(
+                counterFileName, 
+                parseInt(data.toString()) + 1, 
+                function(err){
+                    if(err != null)
+                        throw err;
+                }
+            );
+
+            res.send("current counter value is: " + data);
+        }
+    });
 };
 
+//initIncrementFile();
 //incrementCounterInFile();
+
 
 app.get('/', function(req, res){
     //res.send(incrementCounterInFile());
-    res.send('Hello world');
+    res.send('Hello world2');
+});
+
+app.get('/increment', function(req, res){
+    incrementCounterInFile(res);
+    //res.send('incrementing');
 });
 
 app.listen(3000, function(){
-    console.log('example app listening on port 3000');
-});
+    console.log('Listening on port 3000...');
+}); 
